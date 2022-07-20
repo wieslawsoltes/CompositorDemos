e14b1c1d3b4b13d0ac1a733a2c119c12bf1f6a5c
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2022-07-20 11:41:20 +0200

    First demo

--- CompositorDemos/MainWindow.axaml.cs
+++ CompositorDemos/MainWindow.axaml.cs
@@ -1,6 +1,9 @@
+using System;
+using System.Numerics;
 using Avalonia;
+using Avalonia.Animation;
 using Avalonia.Controls;
-using Avalonia.Markup.Xaml;
+using Avalonia.Rendering.Composition;
 
 namespace CompositorDemos;
 
@@ -12,10 +15,25 @@ public partial class MainWindow : Window
 #if DEBUG
         this.AttachDevTools();
 #endif
+        
+        Border.AttachedToVisualTree += BorderOnAttachedToVisualTree;
     }
 
-    private void InitializeComponent()
+    private void BorderOnAttachedToVisualTree(object? sender, VisualTreeAttachmentEventArgs e)
     {
-        AvaloniaXamlLoader.Load(this);
+        var redVisual = ElementComposition.GetElementVisual(Border);
+        if (redVisual is null)
+        {
+            return;
+        }
+
+        var compositor = redVisual.Compositor;
+
+        var animation = compositor.CreateVector3KeyFrameAnimation();
+        animation.InsertKeyFrame(1f, new Vector3(200f, 0f, 0f));
+        animation.Duration = TimeSpan.FromSeconds(2);
+        animation.Direction = PlaybackDirection.Alternate;
+        animation.IterationCount = 10;
+        redVisual.StartAnimation("Offset", animation);
     }
 }

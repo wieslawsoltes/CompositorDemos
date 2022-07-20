5cce7c79df8acb81f4b13a622157f9eb7026e64c
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2022-07-20 12:23:15 +0200

    Move to page

--- CompositorDemos/MainWindow.axaml.cs
+++ CompositorDemos/MainWindow.axaml.cs
@@ -1,9 +1,5 @@
-using System;
-using System.Numerics;
 using Avalonia;
-using Avalonia.Animation;
 using Avalonia.Controls;
-using Avalonia.Rendering.Composition;
 
 namespace CompositorDemos;
 
@@ -15,28 +11,5 @@ public partial class MainWindow : Window
 #if DEBUG
         this.AttachDevTools();
 #endif
-        
-        Border.AttachedToVisualTree += BorderOnAttachedToVisualTree;
-    }
-
-    private void BorderOnAttachedToVisualTree(object? sender, VisualTreeAttachmentEventArgs e)
-    {
-        var compositionVisual = ElementComposition.GetElementVisual(Border);
-        if (compositionVisual is null)
-        {
-            return;
-        }
-
-        var compositor = compositionVisual.Compositor;
-
-        var animation = compositor.CreateVector3KeyFrameAnimation();
-        animation.InsertKeyFrame(1f, new Vector3(200f, 0f, 0f));
-        animation.Duration = TimeSpan.FromSeconds(2);
-        animation.Direction = PlaybackDirection.Alternate;
-        animation.IterationCount = 10;
-
-        compositionVisual.StartAnimation("Offset", animation);
-        
-        
     }
 }
---
+++ CompositorDemos/Pages/Vector3KeyFrameAnimationPage.axaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Numerics;
+using Avalonia;
+using Avalonia.Animation;
+using Avalonia.Controls;
+using Avalonia.Rendering.Composition;
+
+namespace CompositorDemos.Pages;
+
+public partial class Vector3KeyFrameAnimationPage : UserControl
+{
+    public Vector3KeyFrameAnimationPage()
+    {
+        InitializeComponent();
+
+        
+        Border.AttachedToVisualTree += BorderOnAttachedToVisualTree;
+    }
+
+    private void BorderOnAttachedToVisualTree(object? sender, VisualTreeAttachmentEventArgs e)
+    {
+        var compositionVisual = ElementComposition.GetElementVisual(Border);
+        if (compositionVisual is null)
+        {
+            return;
+        }
+
+        var compositor = compositionVisual.Compositor;
+
+        var animation = compositor.CreateVector3KeyFrameAnimation();
+        animation.InsertKeyFrame(1f, new Vector3(200f, 0f, 0f));
+        animation.Duration = TimeSpan.FromSeconds(2);
+        animation.Direction = PlaybackDirection.Alternate;
+        animation.IterationCount = 10;
+
+        compositionVisual.StartAnimation("Offset", animation);
+    }
+}
+

5737cdc866ad4bf8c759570e81b604f7fcb8b90f
Jumar Macato <16554748+jmacato@users.noreply.github.com>
2022-07-21 20:24:25 +0800

    test implementation

--- CompositorDemos/Pages/ParallaxScrollingPage.axaml.cs
+++ CompositorDemos/Pages/ParallaxScrollingPage.axaml.cs
@@ -1,8 +1,14 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Controls.Generators;
+using Avalonia.Controls.Templates;
 using Avalonia.Interactivity;
+using Avalonia.Rendering.Composition;
+using Avalonia.VisualTree;
 using CompositorDemos.Models;
 
 namespace CompositorDemos.Pages;
@@ -10,35 +16,73 @@ namespace CompositorDemos.Pages;
 public partial class ParallaxScrollingPage : UserControl
 {
     public ParallaxScrollingPage()
-    {       
+    {
         InitializeComponent();
-        ThumbnailList.ItemContainerGenerator.Recycled += ItemContainerGeneratorOnRecycled;
+        ThumbnailList.ItemContainerGenerator.Materialized += ItemContainerGeneratorOnMaterialized;
         ThumbnailList.Items = new List<Thumbnail>
         {
-            new Thumbnail("Land 1", ""),  
-            new Thumbnail("Land 1", ""),  
-            new Thumbnail("Land 1", ""),  
-            new Thumbnail("Land 1", ""),  
-            new Thumbnail("Land 1", ""),  
-            new Thumbnail("Land 1", ""),  
-            new Thumbnail("Land 1", ""),  
-            new Thumbnail("Land 1", ""),  
-            new Thumbnail("Land 1", ""),  
+            new Thumbnail("Land 1", ""),
+            new Thumbnail("Land 1", ""),
+            new Thumbnail("Land 1", ""),
+            new Thumbnail("Land 1", ""),
+            new Thumbnail("Land 1", ""),
+            new Thumbnail("Land 1", ""),
+            new Thumbnail("Land 1", ""),
+            new Thumbnail("Land 1", ""),
+            new Thumbnail("Land 1", ""),
         };
     }
 
-    private void ItemContainerGeneratorOnRecycled(object? sender, ItemContainerEventArgs e)
+    private void ItemContainerGeneratorOnMaterialized(object? sender, ItemContainerEventArgs e)
     {
+        var container = e.Containers[0];
+
+        if (container.ContainerControl is not ListBoxItem lbi)
+        {
+            return;
+        }
+
+        lbi.Loaded += delegate
+        {
+            var image = lbi.GetVisualDescendants().OfType<Image>().First();
+            var scrollViewer = lbi.GetVisualAncestors().OfType<ScrollViewer>().First();
+
+            if (ElementComposition.GetElementVisual(image) is not { } imageComp) return;
+            if (ElementComposition.GetElementVisual(scrollViewer) is not { } svComp) return;
+            if (this.GetVisualRoot() is not Visual visualRoot) return;
+
         
+            var compositionVisualWindow = ElementComposition.GetElementVisual(visualRoot);
+            if (compositionVisualWindow is null)
+            {
+                return;
+            }
+
+            var compositor = compositionVisualWindow.Compositor;
+            
+            var animation = compositor.CreateExpressionAnimation("Window.Size.Y * 0.5 * maskV3");
+            animation.SetVector3Parameter("maskV3", Vector3.UnitY);
+            animation.SetReferenceParameter("Window", compositionVisualWindow);
+            imageComp.StartAnimation("Offset", animation);
+        };
     }
 
     private void Page_Unloaded(object? sender, RoutedEventArgs e)
     {
-        
     }
 
-    private void Page_Loaded(object? sender, RoutedEventArgs e)
+    private void Page_Loaded(object? _, RoutedEventArgs __)
     {
-        
+        // var compositor = ElementComposition.GetElementVisual(this).Compositor;
+        //
+        // // Get scrollviewer
+        // var myScrollViewer = ThumbnailList.GetVisualDescendants().OfType<ScrollViewer>().First();
+        //
+        // // Setup the expression
+        // var scrollPropSet = _scrollProperties.GetSpecializedReference<ManipulationPropertySetReferenceNode>();
+        // var startOffset = ExpressionValues.Constant.CreateConstantScalar("startOffset", 0.0f);
+        // var parallaxValue = 0.5f;
+        // var parallax = (scrollPropSet.Translation.Y + startOffset);
+        // _parallaxExpression = parallax * parallaxValue - parallax;
     }
 }

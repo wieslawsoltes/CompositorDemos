62081170c8a446b1bb1df2cde4429113a7d6e83d
Wiesław Šoltés <wieslawsoltes@users.noreply.github.com>
2022-07-22 09:43:04 +0200

    Update Thumbnails.cs

--- CompositorDemos/Models/Thumbnails.cs
+++ CompositorDemos/Models/Thumbnails.cs
@@ -1,19 +1,3 @@
-using System;
-
 namespace CompositorDemos.Models;
 
-public class Thumbnail
-{
-    public Thumbnail(string name, string url, string description = "")
-    {
-        Name = name;
-        ImageUrl = url;
-        Description = description;
-    }
-
-    public string Name { get; set; }
-
-    public string ImageUrl { get; set; }
-
-    public string Description { get; set; }
-}
+public record Thumbnail(string Name, string ImageUrl, string Description = "");

eade299fd35e10b5858483c8dbdb46c14c0f3848
Wiesław Šoltés <wieslaw.soltes@gmail.com>
2022-08-03 13:48:14 +0200

    Update Program.cs

--- CompositorDemos/Program.cs
+++ CompositorDemos/Program.cs
@@ -12,17 +12,8 @@ class Program
     public static AppBuilder BuildAvaloniaApp()
         => AppBuilder.Configure<App>()
             .UsePlatformDetect()
-            .With(new Win32PlatformOptions
-            {
-                UseCompositor = true
-            })
-            .With(new X11PlatformOptions
-            {
-                UseCompositor = true
-            })
-            .With(new AvaloniaNativePlatformOptions
-            {
-                UseCompositor = true
-            })
+            .With(new Win32PlatformOptions { UseCompositor = true })
+            .With(new X11PlatformOptions { UseCompositor = true })
+            .With(new AvaloniaNativePlatformOptions { UseCompositor = true })
             .LogToTrace();
 }
